9afae52b05ddcc0472ece85a2376176839d9f26b
c <compuprac@gmail.com>
2019-04-19 00:17:19 -0500

    say hi method

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include <iosteam>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
 
+void say_hi(string name){
+    string message = "Hi, " + name;
+    cout << message << endl;
+}
+
+
+int main(){
+    say_hi("Joe");
     return 0;
 }
